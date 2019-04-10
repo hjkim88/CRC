aaa0e171c9c34104f83bb8b2610a96833fd34cea
hjkim88 <firadazer@gmail.com>
2019-04-10 13:24:40 -0400

    completed the implementation.

--- codes/DataTypes.R
+++ codes/DataTypes.R
@@ -21,9 +21,55 @@
 identifyDataTypes <- function(clinInfoPath = "//isilon.c2b2.columbia.edu/ifs/archive/shares/bisr/Parvathi_Myer/data/coadread_tcga_clinical_data.tsv",
                               tcgaFileInfoPath = "//isilon.c2b2.columbia.edu/ifs/archive/shares/bisr/Parvathi_Myer/data///isilon.c2b2.columbia.edu/ifs/archive/shares/bisr/Parvathi_Myer/data/TCGA_COAD_READ_All_Files_Info.txt") {
   
+  ### load the data
+  clinInfo <- read.table(file = clinInfoPath, header = TRUE, sep = "\t",
+                         stringsAsFactors = FALSE, check.names = FALSE)
+  tcgaFileInfo <- read.table(file = tcgaFileInfoPath, header = TRUE, sep = "\t",
+                             stringsAsFactors = FALSE, check.names = FALSE)
   
+  ### there are multiple sample IDs in some of tcgaFileInfo, so be aware of that
   
+  ### get indicies in tcgaFileInfo that overlap with the sample of interest
+  interest_idx <- NULL
+  for(i in 1:nrow(tcgaFileInfo)) {
+    for(j in 1:nrow(clinInfo)) {
+      if(grepl(clinInfo$`Sample ID`[j], tcgaFileInfo$`Sample ID`[i])) {
+        interest_idx <- c(interest_idx, i)
+        break
+      }
+    }
+    if(i %% 100 == 0) {
+      writeLines(paste(i, "/", nrow(tcgaFileInfo)))
+    }
+  }
   
+  ### only retain the info of the 640 samples
+  info_interest <- tcgaFileInfo[interest_idx,]
+  
+  ### get data types of all the available files
+  total_data_types <- unique(info_interest$`Data Type`)
+  
+  ### print out the data types
+  for(i in 1:length(total_data_types)) {
+    writeLines(paste("# of samples for", total_data_types[i], "=", length(which(info_interest$`Data Type` == total_data_types[i]))))
+  }
+  
+  ### Gene Expression Quantification - there are unnormalized & normalized, so we should distinguish them
+  writeLines(paste("# of samples for RNA-Seq counts =",
+                   length(intersect(which(info_interest$`Data Type` == "Gene Expression Quantification"),
+                                    grep("counts", info_interest$`File Name`)))))
+  
+  ### Aligned Reads - WXS, RNA-Seq, and miRNA-Seq
+  writeLines(paste("# of samples for RNA-Seq reads =",
+                   length(intersect(which(info_interest$`Data Type` == "Aligned Reads"),
+                                    grep("rehead", info_interest$`File Name`)))))
+  writeLines(paste("# of samples for miRNA-Seq reads =",
+                   length(intersect(which(info_interest$`Data Type` == "Aligned Reads"),
+                                    grep("mirna", info_interest$`File Name`)))))
+  writeLines(paste("# of samples for WXS reads =",
+                   length(which(info_interest$`Data Type` == "Aligned Reads"))-
+                     length(intersect(which(info_interest$`Data Type` == "Aligned Reads"),
+                                      grep("rehead", info_interest$`File Name`)))-
+                     length(intersect(which(info_interest$`Data Type` == "Aligned Reads"),
+                                      grep("mirna", info_interest$`File Name`)))))
 }
-
-
