4f2428518945f4c7971a4f74e210f169a38b2202
hjkim88 <firadazer@gmail.com>
2019-04-11 16:26:37 -0400

    A skeleton of MatchedNormal.R

---
+++ codes/MatchedNormal.R
@@ -0,0 +1,26 @@
+###
+#   File name : MatchedNormal.R
+#   Author    : Hyunjin Kim
+#   Date      : Apr 11, 2019
+#   Email     : hk2990@cumc.columbia.edu
+#   Purpose   : Identify how many normal Bam files exist in TCGA portal
+#               that are matched to cancer Bam files
+#
+#   Instruction
+#               1. Source("MatchedNormal.R")
+#               2. Run the function "identifyNormals" - specify the file path of the TCGA WXS file info
+#               3. The results will be printed in the console
+#
+#   Example
+#               > source("The_directory_of_MatchedNormal.R/MatchedNormal.R")
+#               > identifyNormals()
+###
+
+identifyNormals <- function() {
+  
+  
+  
+  
+}
+
+
