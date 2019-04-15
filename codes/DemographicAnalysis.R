4b54dba8fd6e985fb0d863a0913721657736eb00
hjkim88 <firadazer@gmail.com>
2019-04-15 11:24:34 -0400

    Skeleton of DemographicAnalysis.R

---
+++ codes/DemographicAnalysis.R
@@ -0,0 +1,35 @@
+###
+#   File name : DemographicAnalysis.R
+#   Author    : Hyunjin Kim
+#   Date      : Apr 15, 2019
+#   Email     : hk2990@cumc.columbia.edu
+#   Purpose   : Make a degraphic table and plots with the TCGA-COAD-READ data
+#
+#   Instruction
+#               1. Source("DemographicAnalysis.R")
+#               2. Run the function "demoAnalysis" - specify TCGA data RDA file path and the output directory
+#               3. The results will be generated under the output directory
+#
+#   Example
+#               > source("The_directory_of_DemographicAnalysis.R/DemographicAnalysis.R")
+#               > demoAnalysis(tcgaDataPath="C:/Research/CUMC/GTExProj/data/RDA_Files/TCGA_33_RAW_COUNTS.rda",
+#                              outputDir="./results/demographic/")
+###
+
+demoAnalysis <- function(tcgaDataPath="//isilon.c2b2.columbia.edu/ifs/archive/shares/af_lab/GTEx/RDA_Files/TCGA_33_RAW_COUNTS.rda",
+                         outputDir="//isilon.c2b2.columbia.edu/ifs/archive/shares/bisr/Parvathi_Myer/results/demographic/") {
+  
+  
+  
+  
+  
+  
+  
+}
+
+
+
+
+
+
+
